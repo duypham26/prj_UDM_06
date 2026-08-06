a31724028c40f8711c0a92c865e94f07d301a005
lenguyennhattruongg <lenguyennhattruongg@gmail.com>
2026-08-06 19:05:53 +0700

    Create protocol structure

---
+++ RemoteDesktop.Shared/Protocol/Packet.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteDesktop.Shared.Protocol
+{
+    internal class Packet
+    {
+    }
+}
