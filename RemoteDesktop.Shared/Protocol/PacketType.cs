a71f66e703c9e72d144af095cb9e7803778ef07d
lenguyennhattruongg <lenguyennhattruongg@gmail.com>
2026-08-06 19:05:53 +0700

    Create protocol structure

---
+++ RemoteDesktop.Shared/Protocol/PacketType.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteDesktop.Shared.Protocol
+{
+    public enum PacketType
+    {
+        //kết nối
+        ConnectRequest,
+        ConnectAccept,
+        ConnectReject,
+        //màn hình
+        Screenframe,
+        //điều khiển chuột
+        MouseMove,
+        MouseDown,
+        MouseUp,
+        MouseWheel,
+        // điều khiển bàn phím
+        KeyDown,
+        KeyUp,
+        // tín hiệu kết nối
+        Disconnect,
+        Heartbeat
+    }
+}
