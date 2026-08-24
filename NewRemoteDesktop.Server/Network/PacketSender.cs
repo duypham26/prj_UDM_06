70953c575d999bd0688d8c387afff6c736c0fa46
VietHoang-262 <garenalienquan420@gmail.com>
2026-08-24 23:03:52 +0700

    Add Server Network and Services modules

---
+++ NewRemoteDesktop.Server/Network/PacketSender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewRemoteDesktop.Server.Network
+{
+    public static class PacketSender
+    {
+        public static byte[] CreatePacket(byte packetType, byte[] payload)
+        {
+            // Định dạng dữ liệu trước khi gửi đi
+            return payload;
+        }
+    }
+}
