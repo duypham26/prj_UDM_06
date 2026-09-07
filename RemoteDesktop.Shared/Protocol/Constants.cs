3b68f9e864718d78dd8bf315723ae8232019b808
lenguyennhattruongg <lenguyennhattruongg@gmail.com>
2026-09-07 12:54:45 +0700

    Finalize shared protocol

--- RemoteDesktop.Shared/Protocol/Constants.cs
+++ RemoteDesktop.Shared/Protocol/Constants.cs
@@ -1,5 +1,4 @@
-<<<<<<< ours
-﻿namespace RemoteDesktop.Shared.Protocol
+namespace RemoteDesktop.Shared.Protocol
 {
     public static class Constants
     {
@@ -10,18 +9,3 @@
         public const int HeartbeatTime = 5000;
     }
 }
-||||||| base
-=======
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RemoteDesktop.Shared.Protocol
-{
-    internal class Constants
-    {
-    }
-}
->>>>>>> theirs
--- RemoteDesktop.Shared/Protocol/Packet.cs
+++ RemoteDesktop.Shared/Protocol/Packet.cs
@@ -1,12 +1,21 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace RemoteDesktop.Shared.Protocol
 {
-    internal class Packet
+    public class Packet
     {
+        public PacketType Type { get; set; }
+
+        public string Data { get; set; }
+
+        public Packet()
+        {
+            Type = PacketType.ConnectRequest;
+            Data = string.Empty;
+        }
+
+        public Packet(PacketType type, string data)
+        {
+            Type = type;
+            Data = data ?? string.Empty;
+        }
     }
 }
